1c238bc1ba4779803136d268a19509bf3cfb7a1c
Becca <rrdoukas@gmail.com>
2020-10-01 19:31:05 -0500

    ads mousedown and mouseup

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -29,9 +29,15 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
-function handlePogress() {
+function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
 }
 
 /* Hook up the event listeners*/
@@ -42,7 +48,14 @@ video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
 );
 ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub);
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
