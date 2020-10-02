385d78a8044e15d9b0168e5f4817b198852b6fbc
Becca <rrdoukas@gmail.com>
2020-10-01 19:06:02 -0500

    adds ranges

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -20,7 +20,15 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
-function skip() {}
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  console.log(this.value);
+  console.log(this.name);
+}
 /* Hook up the event listeners*/
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -28,3 +36,7 @@ video.addEventListener("pause", updateButton);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
